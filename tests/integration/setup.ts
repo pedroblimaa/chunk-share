06e549326fbb9dec5aeca7c70a4fdbc7ce3986a4
Pedro Bonfilio <pedroblimaa@gmail.com>
2026-07-25 14:09:21 -0300

    Add google drive mocks

--- tests/integration/setup.ts
+++ tests/integration/setup.ts
@@ -1,10 +1,29 @@
-import { afterAll, beforeEach } from 'vitest'
+import { setupServer } from 'msw/node'
+import { afterAll, beforeAll, beforeEach } from 'vitest'
+import { createGoogleDriveMockHandlers } from './support/google-drive-mock-handlers'
+import { googleDriveTestEnvironment } from './support/google-drive-test-environment'
 import {
   cleanIntegrationTestStorage,
   configureIntegrationTestStorage
 } from './support/integration-test-storage'
 
-configureIntegrationTestStorage()
+const googleDriveMockServer = setupServer(...createGoogleDriveMockHandlers())
 
-beforeEach(cleanIntegrationTestStorage)
-afterAll(cleanIntegrationTestStorage)
+configureIntegrationTestStorage()
+process.env.CHUNKSHARE_GOOGLE_CLIENT_ID = 'integration-test-client-id'
+process.env.CHUNKSHARE_GOOGLE_CLIENT_SECRET = 'integration-test-client-secret'
+
+beforeAll(() => {
+  googleDriveMockServer.listen({ onUnhandledRequest: 'error' })
+})
+
+beforeEach(async () => {
+  googleDriveMockServer.resetHandlers()
+  googleDriveTestEnvironment.reset()
+  await cleanIntegrationTestStorage()
+})
+
+afterAll(async () => {
+  googleDriveMockServer.close()
+  await cleanIntegrationTestStorage()
+})
